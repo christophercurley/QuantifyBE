afc623c8359e8479dc1cd1cec430f3fd75b0cf69
Christopher Curley <clevercurley@gmail.com>
2024-03-10 15:46:03 -0400

    Create SetDetail Model

---
+++ QuantifyBE/Models/RoutineExerciseDetails.cs
@@ -0,0 +1,17 @@
+﻿namespace QuantifyBE.Models
+{
+    public class RoutineExerciseDetails
+    {
+        public Guid RoutineId { get; set; }
+        public Routine Routine { get; set; }
+
+        public Guid ExerciseId { get; set; }
+        public Exercise Exercise { get; set; }
+
+
+        public int Sets { get; set; }
+        public int Reps { get; set; }
+        public int[] Weights { get; set; }
+        public string Notes { get; set; }
+    }
+}
---
+++ QuantifyBE/Models/SetDetail.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace QuantifyBE.Models
+{
+    public class SetDetail
+    {
+        public Guid Id { get; set; }
+        public Guid RoutineExerciseDetail {  get; set; }
+        public RoutineExerciseDetails RoutineExerciseDetails { get; set;}
+
+        public int SetNumber { get; set; }
+        public int Weight { get; set; }
+        public int Reps { get; set; }
+    }
+}
