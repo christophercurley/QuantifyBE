6954e0190ad5c2bc414fad245799bf342eb3d0df
Christopher Curley <clevercurley@gmail.com>
2024-03-10 15:22:05 -0400

    Create Routine Model

---
+++ QuantifyBE/Models/Routine.cs
@@ -0,0 +1,9 @@
+﻿namespace QuantifyBE.Models
+{
+    public class Routine
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
